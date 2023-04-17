42461e7ab776aa5a06899073a10152b601e42485
chrisho1205 <101864677+chrisho1205@users.noreply.github.com>
2023-04-17 20:53:47 +0800

    pull

--- untitled15.py
+++ untitled15.py
@@ -4,10 +4,9 @@ Created on Thu Dec 29 14:01:26 2022
 
 @author: user
 """
-
 import pyaudio
 import wave
-
+import tkinter as tk
 import time
 from keras.models import load_model
 import librosa
@@ -18,170 +17,153 @@ import matplotlib.pyplot as plt
 import librosa.display
 from scipy.io.wavfile import read
 from scipy.fft import fft
-chunk = 11025 # 記錄聲音的樣本區塊大小
-
+from scipy import signal
+import scipy.io.wavfile
+import  soundfile as sf
+import math
+chunk = 11025 
 labels = [
-            'ambulance',
-            "enviroment",
-            #'fire',
-            'police'
-    ]       
-            
+        'ambulance',
+        'ambulance2',
+        'ambulance3',
+        "enviroment",
+        # 'fire',
+        'police1',
+        'police2'
+        ]    
+data_label=[]     
+data_pred=[]
+data_db=[]
 
-    #warnings.simplefilter("ignore")
-model     = keras.models.load_model("models\\simple-train-nb50&20.hdf5")
-sample_format = (
+#models\\simple_seperate_sec2_new2_retrain5_bandpassfilter50&30.hdf5 音檔未分開
+#models\\simple_seperate_sec2_new2_retrain6_bandpassfilter100&50.hdf5 音檔分開可用
+#7_200&65
+model     = keras.models.load_model("models\\simple_seperate_sec2_new2_retrain6_bandpassfilter100&50.hdf5")
+sample_format = (    
     pyaudio.paInt16
-)  # 樣本格式，可使用 paFloat32、paInt32、paInt24、paInt16、paInt8、paUInt8、paCustomFormat
+)  
+# 樣本格式，可使用 paFloat32、paInt32、paInt24、paInt16、paInt8、paUInt8、paCustomFormat
 channels = 1  # 聲道數量
 fs =22050
 # 取樣頻率，常見值為 44100 ( CD )、48000 ( DVD )、22050、24000、12000 和 11025。
 seconds = 1  # 錄音秒數
-filename = "oxxostudio.wav"  # 錄音檔名
-
 p = pyaudio.PyAudio()  # 建立 pyaudio 物件
-def displaySpectrum(path): # 显示语音频域谱线
-    sr=wave.open(path)
-    print(sr.getframerate())
-    x, sr = librosa.load(path,sr=sr.getframerate())
-    print(len(x))
+def displaySpectrum(path): 
+    #print(sr.getframerate())
+    sr,x=scipy.io.wavfile.read(path)
+    #print(db)
+    #x, sr = librosa.load(path,sr=sr.getframerate())
+    b,a=signal.butter(8,[0.045,0.272],'bandpass')
+    y = signal.filtfilt(b, a, x)
+    #print(len(x))
     # ft = librosa.stft(x)
-    # magnitude = np.abs(ft)  # 对fft的结果直接取模（取绝对值），得到幅度magnitude
-    # frequency = np.angle(ft)  # (0, 16000, 121632)
-
-    ft = fft(x)
+    # magnitude = np.abs(ft) 
+    # frequency = np.angle(ft) 
+    ft = fft(y)
     #print(len(ft), type(ft), np.max(ft), np.min(ft))
-    magnitude = np.absolute(ft)  # 对fft的结果直接取模（取绝对值），得到幅度magnitude
+    magnitude = np.absolute(ft)
     frequency = np.linspace(0, sr, len(magnitude))  # (0, 16000, 121632)
-
-    #print(len(magnitude), type(magnitude), np.max(magnitude), np.min(magnitude))
-    #print(len(frequency), type(frequency), np.max(frequency), np.min(frequency))
-
-    plt.plot(frequency[:], magnitude[:])  # magnitude spectrum
-
+    plt.plot(frequency[:10000], magnitude[:10000])  # magnitude spectrum
     plt.show()
-
-    # # plot spectrum，不限定 [对称]
-    # plt.figure(figsize=(18, 8))
-    # plt.plot(frequency, magnitude)  # magnitude spectrum
-    # plt.title("语音信号频域谱线")
-    # plt.xlabel("频率（赫兹）")
-    # plt.ylabel("幅度")
-    # plt.show()
-
 def displaySpectrogram(path):     
-    
-        x, sr = librosa.load(path)
-    # compute power spectrogram with stft(short-time fourier transform):
-    # 基于stft，计算power spectrogram
-        spectrogram = librosa.amplitude_to_db(librosa.stft(x))
-        
-        print(len(x))
-        # show
-        librosa.display.specshow(spectrogram, x_axis="time",y_axis='log')
-        plt.colorbar(format='%+2.0f dB')
-       
-        plt.show()
-def displaymfccprediction(path):
-            
-    data_label=[]
-    data_pred=[]
-    
-    wf=wave.open(path,"r")
-    rate=wf.getframerate()
-    print(rate)
-    data, sr = librosa.load(path)
-    #print(len(data))
-    #print(sr)
-    #plt.plot(data2)
-    #plt.show()
-    wf.close()
-    mfccs = librosa.feature.mfcc(y=data, sr=sr,n_mfcc=13,dct_type=2,norm='ortho')
-
-    #mfccs = sklearn.preprocessing.scale(mfccs)
-    #print(np.shape(mfccs))
-    normalized_mfcc = librosa.util.normalize(mfccs) #-1~1
-    #normalized_mfcc=np.resize(normalized_mfcc,(13,44))
-    img = librosa.display.specshow(normalized_mfcc,x_axis="time") 
-    mfcc_reshaped = normalized_mfcc.reshape(1, 13, 44, 1)
-    #fig.colorbar(img, ax=ax)
-
+    x, sr = librosa.load(path)
+    spectrogram = librosa.amplitude_to_db(librosa.stft(x))
+    print(len(x))
+    librosa.display.specshow(spectrogram, x_axis="time",y_axis='hz')
+    plt.colorbar(format='%+2.0f dB')
     plt.show()
+def displaymfccprediction(path):      
+    data2, sr = librosa.load(path)
+    print(type(data2[0]))
+    b,a=signal.butter(8,[0.045,0.272],'bandpass')
+    y = signal.filtfilt(b, a, data2)
+    mfccs = librosa.feature.mfcc(y=y, sr=sr,n_mfcc=13,dct_type=2,norm='ortho')
+    normalized_mfcc = librosa.util.normalize(mfccs) #-1~1
+    #img = librosa.display.specshow(normalized_mfcc,x_axis="time") 
+    mfcc_reshaped = normalized_mfcc.reshape(1, 13,87, 1)
+    #fig.colorbar(img, ax=ax)
+    #plt.show()
     pred = model.predict(mfcc_reshaped)
-    print(pred)       
+    print(pred)
+    pred=pred.reshape(5)
+    #print(pred.shape)   
+    #a = tk.StringVar()  # 建立文字變數
     data_pred.append(pred)
+    if(pred[0]+pred[1]+pred[2]>0.1):
+        pred[0]=1
+    if(pred[4]>=0.3):
+        pred[4]=1
+        pred[0]=pred[1]=pred[2]=0
+    np.save("prediction7.npy",data_pred)
+    #print(pred)
     index = np.argmax(pred)
+    #print(index)
     prediction = labels[index]
     print(prediction) # 預測結果
+    
+    #a.set(prediction)
+    #label_url=tk.Label(window,textvariable=a, font=('Arial', 40))
+    #label_url.place(x=0,y=0)     
     data_label.append(prediction)
-    #os.remove(path)
-    
+    np.save("label7.npy",data_label)
 def cut():
-    
-    filepath = path + str(count) + ".wav"
+    #frame=[]
+    filepath = path + str(number) + ".wav"
     wf = wave.open(filepath, "wb")  # 開啟聲音記錄檔
     wf.setnchannels(channels)  # 設定聲道
     wf.setsampwidth(p.get_sample_size(sample_format))  # 設定格式
     wf.setframerate(fs)  # 設定取樣頻率
-    wf.writeframes(b"".join(frames[int(2*(count-1)):int((2*count))]))  # 存檔
+    #frame=frames[int(4*(count-1)):int((4*count))]
+    wf.writeframes(b"".join(frames[int(4*(count-1)):int((4*count))]))  # 存檔
     wf.close()
-    
-   
     displaymfccprediction(filepath)
     #displaySpectrum(filepath)
-    displaySpectrogram(filepath)
-
+    #displaySpectrogram(filepath)
+    #os.remove(filepath)
 print("開始錄音...")
-
-path = "C:\\Users\\Administrator\\Documents\\GitHub\\CNN_Identify_emergencysound\\"
-
+path = "D:\\voice_data\\test2\\"
     # 開啟錄音串流
 count = 1
-start_time=time.time()
+number=0
 stream = p.open(
         format=sample_format,
         channels=channels,
         rate=fs,
         frames_per_buffer=chunk,
         input=True,
-        #input_device_index = 1
     )
-
 frames = []
-j = 0
-#def main()
-"""
-while(1):   
-    data = stream.read(chunk)
-    frames.append(data)
-    #print(len(frames))
-    end_time=time.time()
-    print(end_time-start_time)
-        # 將聲音記錄到串列中
-        #print(i)
-    if len(frames) == 100*count:
-        print(count)
-        cut()
-        count = count + 0.5
-"""
+#start_time=time.time()
 frames = []
-while(count!=5):
+number_p=0
+for i in range(1000):
+    start_time=time.time()
     #frames = []
-    for i in range(0,4):
-        data = stream.read(chunk)
-        frames.append(data)
-        
+   #print(labels)
+    #label_url=tk.Label(window,textvariable=a, font=('Arial', 40))
+    #label_url.place(x=0,y=0)  
+    #a.set("                                                                                 ")
+#for i in range(0,4):
+    data = stream.read(chunk)
+    frames.append(data) 
         #print(len(frames))
         #print(end_time-start_time)
-        if(len(frames)==count*2):
-            end_time=time.time()
-            print(end_time-start_time)
-            cut()
-            count = count + 0.5
+    if(len(frames)==count*4):
+            #label_url=tk.Label(window,text=" ", font=('Arial', 24))
+            #end_time=time.time()
+            #print(end_time-start_time)
+        cut()
+        end_time=time.time()
+        print(end_time-start_time)
+        count = count + 0.5
+        number=number+1
+            #window.update()
+            #print(count)
     #print(len(frames))
 stream.stop_stream()  # 停止錄音
 stream.close()  # 關閉串流
 p.terminate()
-#break
+
 print("錄音結束...")
+#window.mainloop()
 
